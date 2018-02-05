4d11602cd215563cb0a5dc296337fc99be33364a
freddy <34489292+freddy1215@users.noreply.github.com>
2018-02-05 14:09:43 -0700

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,64 @@
+Eneter your name: Chuck
+Hello Chuck
+Enter Hours: 35
+Enter Rate: 2.75
+Pay: 96.25
+
+width = 17
+height = 12.0
+
+width//2
+
+width/2.0
+
+height/3
+
+1 + 2 \* 5
+
+>>> name = "chuck"
+>>> name
+'chuck'
+>>> polite = "How are you today "
+>>> 
+>>> polite
+'How are you today '
+>>> response = "good"
+>>> response
+'good'
+>>> 
+>>> question = "Can i increase my pay"
+>>> question
+'Can i increase my pay'
+>>> huh = " lets see what I pay you"
+>>> huh
+' lets see what I pay you'
+>>> hours = 35
+>>> hourse
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    hourse
+NameError: name 'hourse' is not defined
+>>> hours = " you spent 35 hours hear "
+>>> hourse
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    hourse
+NameError: name 'hourse' is not defined
+>>> hours
+' you spent 35 hours hear '
+>>> rate = " the companies wroth is 2.75 each year"
+>>> rate
+' the companies wroth is 2.75 each year'
+>>>  pay = "you get paid 96.25 how dare you ask for a raise we are in debt right now so no"
+ 
+SyntaxError: unexpected indent
+>>> pay
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    pay
+NameError: name 'pay' is not defined
+>>> Pay = "you get paid 96.25 how dare you ask for a raise we are in debt right now so no"
+>>> Pay
+'you get paid 96.25 how dare you ask for a raise we are in debt right now so no'
+>>> 
+
---
+++ variables-challenge.py
@@ -0,0 +1,13 @@
+Challenge
+Set up your own variable containers:
+
+fill the fav_search_engine variable container with the string "Google"
+fill the year variable container with the number 2015
+fill the next_olympics variable container with the number 2016
+
+
+>>> fav_search_engine = "google"
+>>> variable = 2015
+>>> next_olympics = 2016
+
+>>> fav_search_engine search variable
